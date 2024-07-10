dbe6fd186bcf62acf9e53d346b7d020960ad8cdd
Nahuel <nahuel.94.arrieta@gmail.com>
2024-07-10 20:51:37 -0300

    feat(backend/main): add cors

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    # domains must be specified in the .env
+    allow_origins=[],  # Config setting
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+    allow_headers=["*"],
+)
 
 
 # include API routes
