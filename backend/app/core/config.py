befc3044d938f56fb28f7e154f6cb72751402068
Nahuel <nahuel.94.arrieta@gmail.com>
2024-07-11 21:21:58 -0300

    feat(app/core): add config and add dotenv

---
+++ backend/app/core/config.py
@@ -0,0 +1,16 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+settings = {
+    "database_password": os.getenv("DATABASE_PASSWORD"),
+    "database_username": os.getenv("DATABASE_USERNAME"),
+    "database_host": os.getenv("DATABASE_HOST"),
+    "database_port": os.getenv("DATABASE_PORT"),
+    "database_name": os.getenv("DATABASE_NAME"),
+    "secret_key": os.getenv("SECRET_KEY"),
+    "algorithm": os.getenv("ALGORITHM"),
+    "expire_token": os.getenv("EXPIRE_TOKEN"),
+    "cors_domains": os.getenv("CORS_DOMAINS"),
+}
