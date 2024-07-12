101bf5fea468b68654b9c046aae9bcf4a65343a0
Nahuel <nahuel.94.arrieta@gmail.com>
2024-07-11 21:22:32 -0300

    feat(app/database): add config async database

---
+++ backend/app/core/database.py
@@ -0,0 +1,18 @@
+from core.config import settings
+from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
+from sqlalchemy.orm import DeclarativeBase
+
+SQLALCHEMY_DATABASE_URL = f"postgresql+psycopg://{settings['database_username']}:{settings['databse_password']}@{settings['database_host']}:{settings['database_port']}/{settings['database_name']}"
+
+async_engine = create_async_engine(url=SQLALCHEMY_DATABASE_URL)
+
+AsyncSessionLocal = async_sessionmaker(
+    bind=async_engine,
+    autoflush=False,
+    autocommit=False,
+    future=True,
+)
+
+
+class Base(DeclarativeBase):
+    pass
