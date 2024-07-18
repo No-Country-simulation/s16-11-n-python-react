7a34412a5c93a664646f5d9685202703e26c7718
Nahuel <nahuel.94.arrieta@gmail.com>
2024-07-18 10:41:56 -0300

    feat(app/alembic): add config basic alembic

--- backend/app/alembic/env.py
+++ backend/app/alembic/env.py
@@ -1,13 +1,22 @@
 from logging.config import fileConfig
+from sqlalchemy import pool, engine_from_config
 
-from sqlalchemy import engine_from_config
-from sqlalchemy import pool
+from core.config import settings
+from core.database import Base
 
+"""
+Importar todos los modelos
+"""
 from alembic import context
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+
+config.set_main_option(
+    "sqlalchemy.url",
+    f"postgresql+psycopg://{settings['database_username']}:{settings['databse_password']}@{settings['database_host']}:{settings['database_port']}/{settings['database_name']}",
+)
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
@@ -18,7 +27,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
@@ -64,9 +73,7 @@ def run_migrations_online() -> None:
     )
 
     with connectable.connect() as connection:
-        context.configure(
-            connection=connection, target_metadata=target_metadata
-        )
+        context.configure(connection=connection, target_metadata=target_metadata)
 
         with context.begin_transaction():
             context.run_migrations()
