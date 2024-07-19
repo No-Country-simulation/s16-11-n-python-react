0bc67afd0d2a1ea875129defcae1003c5ef09fd3
Nahuel <nahuel.94.arrieta@gmail.com>
2024-07-19 09:49:04 -0300

    feat(app/api/main): add routes auth and user

--- backend/app/api/main.py
+++ backend/app/api/main.py
@@ -1,9 +1,22 @@
-from api.routes import(
-    hello
-)
-from fastapi import APIRouter
-from api.routes import hello
+from api.routes import user, auth
+from fastapi import APIRouter, status
 
 api_router = APIRouter()
 
-api_router.include_router(hello.router)
+api_router.include_router(
+    auth.router,
+    prefix="/api/auth",
+    tags=["Authentication"],
+    responses={
+        status.HTTP_404_NOT_FOUND: {"description": "Not found"},
+    },
+)
+
+api_router.include_router(
+    user.router,
+    prefix="/api/user",
+    tags=["User"],
+    responses={
+        status.HTTP_404_NOT_FOUND: {"description": "Not found"},
+    },
+)
