af1d225eeb722c983f7532646e7646d73288da81
Nahuel <nahuel.94.arrieta@gmail.com>
2024-07-18 22:37:19 -0300

    merge backend-dev

---
+++ backend/app/api/routes/user.py
@@ -0,0 +1,15 @@
+# from api.dependencies.auth import validate_authentucate_user
+# from api.dependencies.db import get_session
+# from crud.user import UserCrud
+# from fastapi import APIRouter, Depends,HTTPException, status
+# # from schemas.user import
+# from sqlalchemy.ext.asyncio.session import AsyncSession
+# from utils.password import hash
+
+# router = APIRouter()
+
+# @router.post('/',status_code=status.HTTP_201_CREATED)
+# async def create_user(
+#     user_create:s,
+# ):
+#     pass
