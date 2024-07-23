a97943caa292db93609d261e4e22734deffed028
Nahuel <nahuel.94.arrieta@gmail.com>
2024-07-23 09:47:44 -0300

    refactor(app/shcemas/user): refactoring name -> first_name

--- backend/app/schemas/user.py
+++ backend/app/schemas/user.py
@@ -11,14 +11,13 @@ class TokenData(BaseModel):
 
 
 class UserBase(BaseModel):
-    name: str | None = None
+    first_name: str | None = None
     last_name: str | None = None
     email: EmailStr
 
 
 class UserCreate(UserBase):
     password: str
-    # my_course? Ver como asignar al crear
 
 
 class UserSchema(UserBase):
