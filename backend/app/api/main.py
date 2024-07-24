dac28d7e57f9d671e98edbab530b63e6aeade6bb
Nahuel <nahuel.94.arrieta@gmail.com>
2024-07-24 10:54:03 -0300

    feat(app/api/main): add api_route channel

--- backend/app/api/main.py
+++ backend/app/api/main.py
@@ -1,4 +1,4 @@
-from api.routes import user, auth, myCourses
+from api.routes import user, auth, myCourses, channel
 from fastapi import APIRouter, status
 
 api_router = APIRouter()
@@ -25,6 +25,14 @@ api_router.include_router(
     myCourses.router,
     prefix="/api/my_courses",
     tags=["MyCourses"],
+    responses={
+        status.HTTP_404_NOT_FOUND: {"description": "Not found"},
+    },
+)
+api_router.include_router(
+    channel.router,
+    prefix="/api/channel",
+    tags=["Channel"],
     responses={
         status.HTTP_404_NOT_FOUND: {"description": "Not found"},
     },
