913afe77a7d7b0a6becec387db0427275bfd15ba
jovaro <jorgevargas0240@gmail.com>
2024-07-25 16:03:13 -0500

    Feat: Added auth functions

---
+++ frontend/src/services/auth.ts
@@ -0,0 +1,36 @@
+import { API_URL } from '@/utils/config';
+import axios from 'axios';
+
+export const loginUser = async (email: string, password: string) => {
+  try {
+    const formData = new FormData();
+
+    formData.append('username', email);
+    formData.append('password', password);
+
+    const token = await axios.post<LoginResponse>(`${API_URL}/login/`, formData, {
+      headers: { 'content-type': 'application/x-www-form-urlencoded' },
+    });
+
+    return token;
+  } catch (error) {
+    console.error(error);
+    throw new Error('Error at login');
+  }
+};
+
+export const registerUser = async (body: {
+  first_name: string;
+  last_name: string;
+  email: string;
+  password: string;
+}) => {
+  try {
+    const { data } = await axios.post<RegisterResponse>(`${API_URL}/user/`, body);
+
+    return data;
+  } catch (error) {
+    console.error(error);
+    throw new Error('Error at registering');
+  }
+};
