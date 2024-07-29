44fe03c72b3c240f7d22112ce3fca9046c31c5c0
Nahuel <nahuel.94.arrieta@gmail.com>
2024-07-29 10:25:22 -0300

    feat(app/api/routes/channel): add current_user to routes

--- backend/app/api/routes/channel.py
+++ backend/app/api/routes/channel.py
@@ -1,7 +1,7 @@
 from typing import List
 from api.dependencies.auth import validate_authenticate_user
 from api.dependencies.db import get_session
-from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi import APIRouter, Depends, status
 from sqlalchemy.ext.asyncio.session import AsyncSession
 from schemas.channel import ChannelCreate, ChannelSchema
 from crud.channel import ChannelCrud
@@ -17,6 +17,7 @@ router = APIRouter()
 async def create_channel(
     create_channel: ChannelCreate,
     db: AsyncSession = Depends(get_session),
+    current_user: str = Depends(validate_authenticate_user),
 ):
     # exist_channel = await ChannelCrud(db).get_all_by_attribute('')
     new_channel = await ChannelCrud(db).create(create_channel)
@@ -32,6 +33,7 @@ async def create_channel(
 async def get_channel_id(
     channel_id: str,
     db: AsyncSession = Depends(get_session),
+    current_user: str = Depends(validate_authenticate_user),
 ):
     channel = await ChannelCrud(db).get(channel_id)
 
@@ -43,8 +45,9 @@ async def get_channel_id(
     status_code=status.HTTP_200_OK,
     response_model=List[ChannelSchema],
 )
-async def get_channel_id(
+async def get_all_channel(
     db: AsyncSession = Depends(get_session),
+    current_user: str = Depends(validate_authenticate_user),
 ):
     channel = await ChannelCrud(db).get_all()
 
